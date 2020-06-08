f5be48636c35e45fe780dfd00df4d5edaf525ea6
fandongtongxue <admin@fandong.me>
2020-06-08 22:09:48 +0800

    dependency

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/alibaba/HandyJSON.git", from:"4.1.3")
+        .package(url: "https://github.com/kakaopensource/KakaJSON.git", from:"1.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,7 +22,7 @@ let package = Package(
         .target(
             name: "FDFoundation",
             dependencies: [
-            "HandyJSON",
+            "KakaJSON",
         ]),
         .testTarget(
             name: "FDFoundationTests",
