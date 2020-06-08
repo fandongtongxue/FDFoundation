94f3ffa60958f632c4333463f51b6d7b6d86bcd8
fandongtongxue <admin@fandong.me>
2020-06-08 22:01:44 +0800

    FDResponse

--- Sources/FDFoundation/FDFoundation.swift
+++ Sources/FDFoundation/FDFoundation.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 public class FDFoundation : NSObject {
+    //class func
+    public class func getVersion() -> String {
+        return "1.0"
+    }
     
+    public class func getBuildVersion() -> String {
+        return "20200608"
+    }
 }
---
+++ Sources/FDFoundation/FDResponseModel.swift
@@ -0,0 +1,15 @@
+//
+//  FDResponseModel.swift
+//  FDFoundation
+//
+//  Created by bogokj on 2020/6/8.
+//
+
+import Foundation
+import HandyJSON
+
+public class FDResponseModel: HandyJSON {
+    
+    required init() {}
+}
+
