f42c9d6123110baaba7bdda672da3fdb7d8c8d1b
Jane Thompson <339002469@qq.com>
2024-06-05 11:46:15 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/alibaba/HandyJSON", from: "5.0.0"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.1.0"),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
