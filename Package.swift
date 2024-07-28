b4f625cd6e0872e8dbb219734b1cef2d272fb999
Jane Thompson <339002469@qq.com>
2024-07-28 22:03:48 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Miles-Matheson/HandyJSON.git", branch: "master"),
         .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.4.0"),
     ],
     targets: [
