33edc3207d75ec070c0cabdb153de4e0458e3dd9
Jane Thompson <339002469@qq.com>
2024-07-28 22:05:41 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FDFoundation",
-            dependencies: ["HandyJSON","DeviceKit"]),
+            dependencies: ["DeviceKit"]),
         .testTarget(
             name: "FDFoundationTests",
             dependencies: ["FDFoundation"]),
